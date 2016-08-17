d6ece3fd3e9a8e4d28dc78ffc0890db0b4408d13
Bogdan Karpus <bigkrp@gmail.com>
2016-08-17 12:48:02 +0300

    Some refactoring

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,20 @@
-var webpack            = require('webpack');
-var merge              = require('webpack-merge');
-// var NpmInstallPlugin   = require('npm-install-webpack-plugin');
-var ExtractTextPlugin  = require('extract-text-webpack-plugin');
-// var HtmlWebpackPlugin  = require('html-webpack-plugin');
-// var CleanWebpackPlugin = require('clean-webpack-plugin');
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var extractCSS = new ExtractTextPlugin('[name].css');
 
-// var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
-//   template: __dirname + '/dev/index.html',
-//   filename: 'index.html',
-//   inject  : 'body'
-// });
 
 var TARGET = process.env.npm_lifecycle_event;
 
-var common = {
+var webpackConfig = {
   cache  : true,
   debug  : true,
   context: __dirname + '/dev',
   entry  : {
     main: './index.js'
   },
-  // entry: [
-  //     './app/index.js'
-  // ],
   output: {
     path: `${__dirname}/build`,
-    // publicPath: '/',
+    publicPath: '/',
     filename: '[name].js',
     chunkFilename: '[id].js',
     sourceMapFilename: '[file].map'
@@ -34,18 +22,13 @@ var common = {
   resolve: {
     extensions: ['', '.js', '.jsx', '.scss']
   },
-  // devtool: 'eval-source-map',
   module: {
     loaders: [{
       test: /\.jsx?$/,
       include: `${__dirname}/dev`,
       loader: 'babel-loader'
     }, {
-      test: /\.css$/,
-      loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
-    }, {
       test: /\.scss$/i,
-      // loader: ExtractTextPlugin.extract('style', 'css?sourceMap!sass?sourceMap'),
       loader: extractCSS.extract(['css?sourceMap','sass?sourceMap'])
     }, {
       test: /\.woff2?$/,
@@ -56,40 +39,18 @@ var common = {
     }]
   },
   plugins: [
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery'
-    }),
     extractCSS
-    // new ExtractTextPlugin('[name].css')
   ]
 };
 
 if (TARGET === 'dev' || !TARGET) {
-  module.exports = merge(common, {
-    devtool: 'cheap-module-source-map',
-    devServer: {
-      port: 8090,
-      historyApiFallback: true,
-      // hot: false,
-      contentBase: `${__dirname}/build`
-    },
-    // watch: true,
-    // plugins: [
-    //   new NpmInstallPlugin({
-    //     save: true // --save
-    //   })
-    // ]
-  });
+  webpackConfig.plugins.push(
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  );
+  webpackConfig.devtool = 'cheap-module-source-map';
 }
 if (TARGET === 'build') {
-  module.exports = merge(common, {
-    devtool: 'source-map'
-    // output: {
-    //   path: `${__dirname}/dist`
-    // },
-    // plugins: [
-    //   HTMLWebpackPluginConfig
-    // ]
-  });
+  webpackConfig.devtool = 'source-map';
 }
+module.exports = webpackConfig;
